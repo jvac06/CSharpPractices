763bf0aa5f0102d0e42274acddf4a9b67c974a58
jesidacosta <jvac06@gmail.com>
2020-02-09 09:37:37 -0500

    Entered advanced conditions Ch19

--- Program.cs
+++ Program.cs
@@ -339,47 +339,131 @@ namespace CSharpPractices
                        Console.WriteLine("Always Down: " + alwaysDown);
                        Console.WriteLine("Always Up: " + alwaysUp);
            */
-            //Ch11 - Calculations with Dates
+        /*            //Ch11 - Calculations with Dates
 
+                    try
+                    {
+                        // Text input of date
+                        Console.Write("Enter date: ");
+                        string input = Console.ReadLine();
+                        // Conversion to DateTime object
+                        DateTime enteredDate = Convert.ToDateTime(input);
+                        // Some calculations
+                        DateTime followingDay = enteredDate.AddDays(1);
+                        DateTime previousDay = enteredDate.AddDays(-1);
+                        // Outputs
+                        Console.WriteLine();
+                        Console.WriteLine("Entered day : " + enteredDate.
+                        ToLongDateString());
+                        Console.WriteLine("Following day: " + followingDay.
+                        ToLongDateString());
+                        Console.WriteLine("Previous day : " + previousDay.
+                        ToLongDateString());
+
+                        //Extracting Quarter of entered date
+                        int enteredMonth = enteredDate.Month;
+                        int enteredYear = enteredDate.Year;
+
+                        int quarter = (enteredMonth + 2) / 3;
+
+                        Console.WriteLine("Quarter of Entered Date: " + quarter);
+
+                    }
+                    catch (Exception)
+                    {
+                        // Treating incorrect input
+                        Console.WriteLine("Incorrect input");
+                    }
+
+                    //Use TimeSpan object to do timespan calculations between two dates
+
+                    //Waiting for Enter
+                    Console.ReadLine();
+                    */
+
+            //Chapter 19
+            //PREPARATIONS
+            string errorMessage = "Incorrect input";
+            int mathematics, informationTechnology, science, english;
+            bool hasUnexcusedAbsences;
+
+            //INPUTS
             try
             {
-                // Text input of date
-                Console.Write("Enter date: ");
+                Console.WriteLine("Enter grades from individual subjects:");
+
+                Console.Write("Mathematics: ");
                 string input = Console.ReadLine();
-                // Conversion to DateTime object
-                DateTime enteredDate = Convert.ToDateTime(input);
-                // Some calculations
-                DateTime followingDay = enteredDate.AddDays(1);
-                DateTime previousDay = enteredDate.AddDays(-1);
-                // Outputs
-                Console.WriteLine();
-                Console.WriteLine("Entered day : " + enteredDate.
-                ToLongDateString());
-                Console.WriteLine("Following day: " + followingDay.
-                ToLongDateString());
-                Console.WriteLine("Previous day : " + previousDay.
-                ToLongDateString());
-
-                //Extracting Quarter of entered date
-                int enteredMonth = enteredDate.Month;
-                int enteredYear = enteredDate.Year;
-
-                int quarter = (enteredMonth + 2) / 3;
-
-                Console.WriteLine("Quarter of Entered Date: " + quarter);
-
+                mathematics = Convert.ToInt32(input);
+                if (mathematics < 1 || mathematics > 5)
+                {
+                    Console.WriteLine(errorMessage);
+                    return;
+                }
+                Console.Write("Information technology: ");
+                input = Console.ReadLine();
+                informationTechnology = Convert.ToInt32(input);
+                if (informationTechnology < 1 || informationTechnology > 5)
+                {
+                    Console.WriteLine(errorMessage);
+                    return;
+                }
+                Console.Write("Science: ");
+                input = Console.ReadLine();
+                science = Convert.ToInt32(input);
+                if (science < 1 || science > 5)
+                {
+                    Console.WriteLine(errorMessage);
+                    return;
+                }
+                Console.Write("English: ");
+                input = Console.ReadLine();
+                english = Convert.ToInt32(input);
+                if (english < 1 || english > 5)
+                {
+                    Console.WriteLine(errorMessage);
+                    return;
+                }
+                Console.Write("Any unexcused absences? (yes/no): ");
+                input = Console.ReadLine();
+                input = input.ToLower();
+                if (input != "yes" && input != "no")
+                {
+                    Console.WriteLine(errorMessage);
+                    return;
+                }
+                hasUnexcusedAbsences = input == "yes";
             }
             catch (Exception)
             {
-                // Treating incorrect input
-                Console.WriteLine("Incorrect input");
+                Console.WriteLine(errorMessage);
+                return;
             }
 
-            //Use TimeSpan object to do timespan calculations between two dates
+            //EVALUATIONS
+            double average = (mathematics + informationTechnology + science + english) / 4.0;
+            string message;
 
-            //Waiting for Enter
-            Console.ReadLine();
-
+            //testing for EXCELLENCE
+            if (average< 1.5001 && mathematics <= 2 && informationTechnology <= 2 && science <= 2
+                && english <= 2 && !hasUnexcusedAbsences)
+            {
+                message = "Excellent";
+            }
+            else
+            {
+                //testing if good
+                if (mathematics == 5|| informationTechnology ==5 || science == 5 || english == 5)
+                {
+                    message = "Failed";
+                }
+                else
+                {
+                    message = "Good";
+                }
+            }
+            //OUTPUT
+            Console.WriteLine("Summary Evaluation: " + message);
         }
     }
 }
